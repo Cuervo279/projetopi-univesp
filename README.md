c6912d3debe8569bfdbeccf63207e4abc9ec40e4
Leonardo Cuervo <115048855+Cuervo279@users.noreply.github.com>
2025-03-11 21:23:27 -0300

    Subindo as pastas do projeto (Beta)
    Frontend

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# projetopi-univesp
+# Rodar o projeto
+
+-npm run dev
+
+# Frontend - React + TypeScript + Vite
+
+node -v v22.13.1
+npm -v 11.1.0
+vite (atual)
---
+++ src/MainContent.tsx
@@ -0,0 +1,106 @@
+import { useState, useEffect } from 'react';
+import './MainContent.css';
+
+function MainContent() {
+
+return (
+
+    <section className='gallery'>
+        <div >
+            <ul className="gallery-item">
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+            </ul>
+            <ul className="gallery-item">
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+            </ul>
+            <ul className="gallery-item">
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+            </ul>
+            <ul className="gallery-item">
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+            </ul>
+            <ul className="gallery-item">
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+                <li className="gallery-div">
+                    <div className="icon-gallery"></div>
+                    <p>Nome do Jogo</p>
+                </li>
+            </ul>
+        </div>
+    </section>
+);
+
+}
+
+    export default MainContent;
